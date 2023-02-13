1d63e846dcf50f16e106366f57b3e036c0975b4d
Silvina Denton <dentonsilvina@hotmail.com>
2023-02-12 23:08:56 -0300

    finlice foto

---
+++ js/eventos-datos.js
@@ -0,0 +1,14 @@
+let boton_email=document.getElementById("Email");
+
+function email(params){
+    
+       
+    document.getElementById('Email').innerHTML="susana@hotmail.com";
+   
+          
+    
+}
+function no_email(){
+
+    document.getElementById('Email').inner;
+}
